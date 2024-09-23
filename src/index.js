e356683a5e943cc891da5caf26d4a56d6622710b
Alex Kozhenov <kozhenov1984@mail.ru>
2024-09-23 05:08:47 +0400

    add solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let listItems = [];
+  let index = 0;
+  let count = 0;
+  let num = 0;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    listItems[i] = bracketsConfig[i].join("");
+  }
+
+  while (count < listItems.length) {
+    while (num < str.length) {
+      index = str.indexOf(listItems[count]);
+      if (index !== -1) {
+        str =
+          str.substring(0, index) +
+          str.substring(index + 2, str.length);
+        count = 0;
+      } else {
+        num = str.length;
+      }
+    }
+    count++;
+    num = 0;
+  }
+  return !str;
+};
